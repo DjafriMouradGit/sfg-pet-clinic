9fb86dc64f1b7f368049ffe57e07f8b4debf7e51
DjafriMouradGit <djafrimourad.jobs@gmail.com>
2020-04-27 01:33:34 +0200

    Add POJO. closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.model;
+import java.util.Date;
+
+public class Pet{
+     private PetTpe petTpe;
+     private Owner owner;
+     private Date birthDate;
+
+    public PetTpe getPetTpe() {
+        return petTpe;
+    }
+
+    public void setPetTpe(PetTpe petTpe) {
+        this.petTpe = petTpe;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
